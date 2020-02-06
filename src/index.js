75268027101929e0851ec054e16f63a6be727764
Dawid Konojacki <dawid.konojacki@sb-betting.com>
2020-02-06 15:16:45 +0100

    css-loader , url-loader and image-webpack-loader for images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import sum from './sum';
 import './image_viewer';
+import '../styles/styles.scss';
 
 const total = sum(2, 15);
 console.log(total);
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 
 const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: 'build/'
     },
+    plugins: [
+        new MiniCssExtractPlugin()
+    ],
     module: {
         rules: [
             {
+                test: /\.s?css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
                 test: /\.js$/,
                 use: 'babel-loader'
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 50000
+                        }
+                    },
+                    'image-webpack-loader'
+                ]
             }
         ]
     }
